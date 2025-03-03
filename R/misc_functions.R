df96c56ab2eed095c319781508eb859fd8dd82b9
jibarozzo <bolaponte@pm.me>
2025-03-03 16:32:31 -0600

    Added new `subset.fasta` function in `misc_function.R`. It's a borrowed function from https://github.com/GuillemSalazar/FastaUtils/blob/master/R/FastaUtils_functions.R. Core and non-core fasta files generated and incorporated to repo.

--- R/misc_functions.R
+++ R/misc_functions.R
@@ -36,3 +36,27 @@ ExtractMatrix <- function(physeq, .vec, keep_rows_sums = 0) {
 
   # cli::cli_alert_success("Yay! You got a matrix.")
 }
+
+
+
+# Borrowed subset.fasta from https://github.com/GuillemSalazar/FastaUtils/blob/master/R/FastaUtils_functions.R
+
+#' Select a subset of sequences from a fasta file
+#'
+#' This function loads a fasta file, selects a subset on sequences based on the sequence's names and writes a new fsata file.
+#' @param file Fasta file
+#' @param subset Vector with (exact) names of the sequences to be retrieved.
+#' @param out Path to output file. If absent, the '.mod' is added to the input file's name (path is thus conserved).
+#' @keywords FastaUtils
+#' @return Writes a fasta file with the selected sequences.
+#' @export
+#' @author Guillem Salazar <salazar@@icm.csic.es>
+#' @examples
+#' subset.fasta(file = "http://greengenes.lbl.gov/Data/JD_Tutorial/UnAligSeq24606.txt", subset = c("24.6jsd1.Tut", "24.6jsd2.Tut ", "24.6jsd3.Tut "), out = "out.fasta")
+subset.fasta <- function(file = NULL, subset = NULL, out = paste(file, ".subset", sep = "")) {
+  library(Biostrings)
+  sequences <- readDNAStringSet(file)
+  if (all(as.character(subset) %in% names(sequences)) == FALSE) stop("There are names in 'subset' not present in the fasta file")
+  pos <- match(as.character(subset), names(sequences))
+  writeXStringSet(sequences[pos], filepath = out)
+}
