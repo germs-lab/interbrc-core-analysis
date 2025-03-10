afd452bbafe8ea605c385a5ee91b7ba81b4d6b3c
jibarozzo <bolaponte@pm.me>
2025-03-10 12:27:15 -0500

    Gains NMDS plots for core and non_core ASV communities. New mmiscellaneous function for creating a basic NMDS plot.

--- R/006_core_ordinations.R
+++ R/006_core_ordinations.R
@@ -6,6 +6,8 @@
 source("R/000_setup.R")
 load("data/output/core_summary_lists.rda")
 load("data/output/phyloseq_objects/filtered_phyloseq.rda")
+load("data/output/phyloseq_objects/core_brc_phyloseq.rda")
+load("data/output/phyloseq_objects/non_core_brc_phyloseq.rda")
 load("data/output/core_asv_matrix.rda")
 load("data/output/non_core_asv_matrix.rda")
 
@@ -24,60 +26,126 @@ core_hell_matrix <- decostand(t(core_asv_matrix),
 ###########
 
 # Ordinations
-core_asv_dist <- vegdist(t(core_hell_matrix), method = "bray", upper = FALSE, binary = FALSE, na.rm = TRUE)
+core_asv_dist <- vegdist(t(core_hell_matrix),
+  method = "bray",
+  upper = FALSE,
+  binary = FALSE,
+  na.rm = TRUE
+)
+
 
 ## Choosing the number of dimensions
-set.seed(484035)
-nmds_screen_parallel(core_asv_dist, ncores = 32) #  Results: Two dimensions keeps stress below 0.20
+# set.seed(484035)
+# nmds_screen_parallel(core_asv_dist, ncores = 32) # Results: Two dimensions keeps stress below 0.20
 
-NMDS <- metaMDS(as.matrix(core_asv_dist),
+## Core Community
+core_ordi <- metaMDS(as.matrix(core_asv_dist),
   distance = "bray",
-  display = c("sites", "species"),
+  display = c("sites"),
   noshare = TRUE,
   autotransform = FALSE,
   wascores = TRUE,
   tidy = TRUE,
   k = 2,
-  trymax = 500,
+  trymax = 100,
   parallel = parallel::detectCores()
 )
 
-stressplot(NMDS)
+stressplot(core_ordi)
 
 # Adding site scores to `NMDS`
-sppscores(NMDS) <- core_hell_matrix
+vegan::sppscores(core_ordi) <- t(core_hell_matrix)
 
 ## Scores and sample data for NMDS
-nmds_scores <- as_tibble(vegan::scores(NMDS)$sites)
-core_brc_sample_df <- core_brc_phyloseq@sam_data |>
-  data.frame()
+core_nmds_scores <- as.data.frame(vegan::scores(core_ordi)$sites)
+
+rownames(core_nmds_scores) <- rownames(vegan::scores(core_ordi)$sites)
+
+core_nmds_scores <- core_nmds_scores %>%
+  rownames_to_column(., var = "unique_id")
+
+core_brc_sample_df <- core_brc_phyloseq@sam_data %>%
+  data.frame() %>%
+  rownames_to_column(., var = "unique_id")
+
+core_nmds_df <- right_join(core_brc_sample_df, core_nmds_scores, by = "unique_id")
 
 
-# NMDS Aesthetics ####
+# Core NMDS Aesthetics ####
+core_nmds_crops <- core_nmds(
+  .data = core_nmds_df,
+  .color = crop,
+  .drop_na = brc
+)
 
-ggplot(nmds_scores, x = NMDS1, y = NMDS2) +
-  geom_point(data = nmds_scores, aes(size = 3, , color = core_brc_sample_df$brc, alpha = 0.5, stroke = 1))
-
-
-
-scale_shape_manual(values = c(15:22)) +
-  # scale_color_manual(values = met.brewer(name = "Nizami", n = 8, type="discrete")) +
-  geom_hline(yintercept = 0, colour = "grey50", size = 0.65) +
-  geom_vline(xintercept = 0, colour = "grey50", size = 0.65) +
-  theme_bw(base_size = 12) +
-  theme(
-    legend.position = "right", legend.title = element_text(),
-    legend.text = element_text(face = "italic")
-  ) +
-  stat_ellipse(
-    mapping = NULL, data = nmds_scores, geom = "path", size = 1.3,
-    position = "identity", type = "t", linetype = 1, level = 0.95, segments = 51,
-    na.rm = FALSE, show.legend = NA, inherit.aes = TRUE
-  )
-
-NM.DS
+core_nmds_brc <- core_nmds(
+  .data = core_nmds_df,
+  .color = brc,
+  .drop_na = brc
+)
 
 
 
-# Print the plot
-print(nmds_plot)
+## Non-Core community
+
+# Hellinger transformation of matrices
+# To be used for NMDS, dbRDA and adonis2
+non_core_hell_matrix <- decostand(t(non_core_asv_matrix),
+  MARGIN = 1,
+  method = "hellinger"
+) # Now we need samples as columns and ASV are rows
+
+###########
+
+# Ordinations
+non_core_asv_dist <- vegdist(t(non_core_hell_matrix),
+  method = "bray",
+  upper = FALSE,
+  binary = FALSE,
+  na.rm = TRUE
+)
+
+## Core Community
+non_core_ordi <- metaMDS(as.matrix(non_core_asv_dist),
+  distance = "bray",
+  display = c("sites"),
+  noshare = TRUE,
+  autotransform = FALSE,
+  wascores = TRUE,
+  tidy = TRUE,
+  k = 2,
+  trymax = 00,
+  parallel = parallel::detectCores()
+)
+
+stressplot(core_ordi)
+
+# Adding site scores to `NMDS`
+vegan::sppscores(non_core_ordi) <- t(non_core_hell_matrix)
+
+## Scores and sample data for NMDS
+non_nmds_scores <- as.data.frame(vegan::scores(non_core_ordi)$sites)
+
+rownames(non_nmds_scores) <- rownames(vegan::scores(non_core_ordi)$sites)
+
+non_nmds_scores <- non_nmds_scores %>%
+  rownames_to_column(., var = "unique_id")
+
+non_core_brc_sample_df <- non_core_brc_phyloseq@sam_data %>%
+  data.frame() %>%
+  rownames_to_column(., var = "unique_id")
+
+non_core_nmds_df <- right_join(non_core_brc_sample_df, non_nmds_scores, by = "unique_id")
+
+# Non-Core NMDS Aesthetics ####
+non_core_nmds_crops <- core_nmds(
+  .data = core_nmds_df,
+  .color = crop,
+  .drop_na = brc
+)
+
+non_core_nmds_brc <- core_nmds(
+  .data = core_nmds_df,
+  .color = brc,
+  .drop_na = brc
+)
