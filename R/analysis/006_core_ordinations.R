12fa89b0f3d88946a2ea32911d113d1dfeb44c3f
jibarozzo <bolaponte@pm.me>
2025-03-12 16:48:04 -0500

    NMDS for core ASVs obtained at 60% threshold.

--- R/analysis/006_core_ordinations.R
+++ R/analysis/006_core_ordinations.R
@@ -10,15 +10,18 @@ load("data/output/phyloseq_objects/core_brc_phyloseq.rda")
 load("data/output/phyloseq_objects/non_core_brc_phyloseq.rda")
 load("data/output/core_asv_matrix.rda")
 load("data/output/non_core_asv_matrix.rda")
+load("data/output/phyloseq_objects/core_asvs_threshold.rda")
+load("data/output/high_occ_matrix.rda")
+load("data/output/low_occ_matrix.rda")
 
 
 #################################################
 ###### Ordinations and Community Analysis #######
 #################################################
 
-############
-### Core ###
-############
+##############################
+### Core by ExctractCore() ###
+##############################
 # Hellinger transformation of matrices
 # To be used for NMDS, dbRDA and adonis2
 core_hell_matrix <- decostand(t(core_asv_matrix),
@@ -107,32 +110,10 @@ ggsave(
   units = "mm"
 )
 
-# Threshold analysis
 
-# Calculate occurrence of each ASV across samples
-asv_sample_counts <- colSums(core_asv_matrix > 0)
-
-# Get total number of samples
-total_samples <- nrow(core_asv_matrix)
-
-# Define occurrence threshold (60% of samples)
-threshold_60 <- total_samples * 0.6
-
-# Select ASVs based on occurrence criteria
-high_occurrence_asvs <- names(asv_sample_counts[asv_sample_counts >= threshold_60])
-low_occurrence_asvs <- names(asv_sample_counts[asv_sample_counts < threshold_60])
-
-# Print ASV counts
-cat("ASVs found in ≥60% samples:", length(high_occurrence_asvs), "\n")
-cat("ASVs found in <60% samples:", length(low_occurrence_asvs), "\n")
-
-
-
-
-
-################
-### Non-Core ###
-################
+##################################
+### Non-Core by ExctractCore() ###
+##################################
 
 # Hellinger transformation of matrices
 non_core_hell_matrix <- decostand(t(non_core_asv_matrix),
@@ -217,3 +198,99 @@ ggsave(
   height = 200,
   units = "mm"
 )
+
+
+######################################
+### Core and Non-Core by Threshold ###
+######################################
+
+
+# Perform NMDS on selected ASVs
+
+# Hellinger transformation of matrices
+# High Prevallen/Occupancy
+core_hell_matrix <- decostand(t(physeq_high_occ_matrix),
+  MARGIN = 1,
+  method = "hellinger"
+) # Now we need samples as columns and ASV are rows
+
+###########
+
+# Ordinations
+core_asv_dist <- vegdist(t(core_hell_matrix),
+  method = "bray",
+  upper = FALSE,
+  binary = FALSE,
+  na.rm = TRUE
+)
+
+## Choosing the number of dimensions
+# set.seed(484035)
+# nmds_screen_parallel(core_asv_dist, ncores = 32) # Results: Two dimensions keeps stress below 0.20
+
+core_ordi <- metaMDS(as.matrix(core_asv_dist),
+  distance = "bray",
+  display = c("sites"),
+  noshare = TRUE,
+  autotransform = FALSE,
+  wascores = TRUE,
+  tidy = TRUE,
+  k = 2,
+  trymax = 100,
+  parallel = parallel::detectCores()
+)
+
+stressplot(core_ordi)
+
+
+## Scores and sample data for NMDS
+vegan::sppscores(core_ordi) <- t(core_hell_matrix)
+
+core_nmds_scores <- as.data.frame(vegan::scores(core_ordi)$sites)
+
+rownames(core_nmds_scores) <- rownames(vegan::scores(core_ordi)$sites)
+
+core_nmds_scores <- core_nmds_scores %>%
+  rownames_to_column(., var = "unique_id")
+
+core_brc_sample_df <- physeq_high_occ@sam_data %>%
+  data.frame() %>%
+  rownames_to_column(., var = "unique_id")
+
+core_nmds_df <- right_join(core_brc_sample_df, core_nmds_scores, by = "unique_id")
+
+
+# Core NMDS Aesthetics ####
+core_nmds_crops <- core_nmds(
+  .data = core_nmds_df,
+  .color = crop,
+  .drop_na = brc
+) + ggtitle("50 core ASVs in BRC crops (100% samples)")
+
+core_nmds_brc <- core_nmds(
+  .data = core_nmds_df,
+  .color = brc,
+  .drop_na = brc
+) + ggtitle("50 core ASVs in BRCs (100% samples)")
+
+
+
+# nmds_high_occ_selected <- ordinate(physeq_high_occ, method = "NMDS", distance = "bray", trymax = 100, zerodist <- "add")
+# nmds_low_occ_selected <- ordinate(physeq_low_occ, method = "NMDS", distance = "bray", trymax = 100)
+
+# # Plot NMDS results
+# nmds_plot_high_occ_selected <- plot_ordination(ps_high_occ_selected, nmds_high_occ_selected, color = "Plant", shape = "Location") +
+#   geom_point(size = 3, alpha = 0.8) +
+#   # facet_wrap(~Date, ncol = 2) +
+#   labs(title = "NMDS of Selected Highly Prevalent ASVs", x = "NMDS1", y = "NMDS2") +
+#   theme_minimal()
+
+# nmds_plot_low_occ_selected <- plot_ordination(ps_low_occ_selected, nmds_low_occ_selected, color = "Plant", shape = "Location") +
+#   geom_point(size = 3, alpha = 0.8) +
+#   # facet_wrap(~Date, ncol = 2) +
+#   labs(title = "NMDS of Selected Low Prevalence ASVs", x = "NMDS1", y = "NMDS2") +
+#   theme_minimal()
+
+# # Print NMDS plots
+# print(nmds_plot_high_occ_selected)
+# print(nmds_plot_low_occ_selected)
--- tests/test_analysis.R
+++ tests/test_analysis.R
@@ -1,15 +1,15 @@
 # Setup
-source("R/000_setup.R")
+source("R/utils/000_setup.R")
 load(
-    "tests/data/test_phyloseq.rda"
+  "tests/data/test_phyloseq.rda"
 )
 
 # Test data set
 ## Making it small to it can run fast
 
 test_small_phyloseq <- test_phyloseq %>%
-    prune_taxa(taxa_sums(.) > 5000 & taxa_sums(.) < 50000, .) %>%
-    prune_samples(sample_sums(.) >= 1, .)
+  prune_taxa(taxa_sums(.) > 5000 & taxa_sums(.) < 50000, .) %>%
+  prune_samples(sample_sums(.) >= 1, .)
 
 # test_large_phyloseq <- prune_samples(sample_sums(test_phyloseq) >= 20000, test_phyloseq)
 
@@ -27,79 +27,79 @@ BC_ranked_max <- BC_ranked[1:max, ]
 
 
 BC_ranked_max %>%
-    ggplot(aes(x = rank[1:max], y = proportionBC)) +
-    geom_point(
-        pch = 21,
-        col = "black",
-        alpha = 0.85,
-        size = 3.5
-    ) +
-    geom_vline(
-        xintercept = last(as.numeric(BC_ranked$rank[(BC_ranked$IncreaseBC >= 1.02)])),
-        lty = 4,
-        col = "darkred",
-        cex = 0.5
-    ) +
-    scale_x_discrete(
-        limits = BC_ranked$rank[1:max], # making the x axis more readable
-        breaks = BC_ranked$rank[1:max][seq(1, length(BC_ranked$rank[1:max]),
-                                           by =
-                                               10
-        )]
-    ) +
-    xlab("Ranked OTUs") +
-    ylab("Contribution to Bray-Curtis Similarity") +
-    annotate(
-        geom = "text",
-        x = last(as.numeric(BC_ranked$rank[(BC_ranked$IncreaseBC >= 1.02)])),
-        y = 0.18,
-        label = paste("Last 2% increase\n(", last(as.numeric(BC_ranked$rank[(BC_ranked$IncreaseBC >=
-                                                                                 1.02)])), " OTUs)", sep = ""),
-        color = "darkred",
-        size = 4,
-    ) +
-    theme_bw() +
-    theme(
-        axis.title.x = element_text(size = 14),
-        title = element_text(size = 14),
-        axis.title.y = element_text(size = 14),
-        strip.text.x = element_text(size = 10),
-        strip.text.y = element_text(size = 14),
-        axis.text.x = element_text(size = 10, color = "black"),
-        axis.text.y = element_text(size = 12, color = "black"),
-        legend.text = element_text(size = 12),
-        legend.title = element_text(size = 14),
-        plot.margin = unit(c(.5, 1, .5, .5), "cm")
-    )
+  ggplot(aes(x = rank[1:max], y = proportionBC)) +
+  geom_point(
+    pch = 21,
+    col = "black",
+    alpha = 0.85,
+    size = 3.5
+  ) +
+  geom_vline(
+    xintercept = last(as.numeric(BC_ranked$rank[(BC_ranked$IncreaseBC >= 1.02)])),
+    lty = 4,
+    col = "darkred",
+    cex = 0.5
+  ) +
+  scale_x_discrete(
+    limits = BC_ranked$rank[1:max], # making the x axis more readable
+    breaks = BC_ranked$rank[1:max][seq(1, length(BC_ranked$rank[1:max]),
+      by =
+        10
+    )]
+  ) +
+  xlab("Ranked OTUs") +
+  ylab("Contribution to Bray-Curtis Similarity") +
+  annotate(
+    geom = "text",
+    x = last(as.numeric(BC_ranked$rank[(BC_ranked$IncreaseBC >= 1.02)])),
+    y = 0.18,
+    label = paste("Last 2% increase\n(", last(as.numeric(BC_ranked$rank[(BC_ranked$IncreaseBC >=
+      1.02)])), " OTUs)", sep = ""),
+    color = "darkred",
+    size = 4,
+  ) +
+  theme_bw() +
+  theme(
+    axis.title.x = element_text(size = 14),
+    title = element_text(size = 14),
+    axis.title.y = element_text(size = 14),
+    strip.text.x = element_text(size = 10),
+    strip.text.y = element_text(size = 14),
+    axis.text.x = element_text(size = 10, color = "black"),
+    axis.text.y = element_text(size = 12, color = "black"),
+    legend.text = element_text(size = 12),
+    legend.title = element_text(size = 14),
+    plot.margin = unit(c(.5, 1, .5, .5), "cm")
+  )
 
 # Plot Abundance Occupancy curve
 occ_abun <- spatial_core[[4]]
 
 occ_abun %>%
-    ggplot(aes(
-        x = log10(otu_rel),
-        y = otu_occ,
-        fill = fill
-    )) +
-    scale_fill_npg(
-        name = "Core Membership",
-        labels = c("Core Taxa", "Non Core Taxa")
-    ) +
-    geom_point(pch = 21, alpha = 1, size = 2.5) +
-    labs(x = "log10(Mean Relative Abundance)", y = "Occupancy") +
-    theme_bw() +
-    theme(
-        axis.title.x = element_text(size = 14),
-        title = element_text(size = 14),
-        axis.title.y = element_text(size = 14),
-        strip.text.x = element_text(size = 10),
-        strip.text.y = element_text(size = 14),
-        axis.text.x = element_text(size = 12, color = "black"),
-        axis.text.y = element_text(size = 12, color = "black"),
-        legend.text = element_text(size = 12),
-        legend.title = element_text(size = 14),
-        plot.margin = unit(c(.5, 1, .5, .5), "cm")
-    )
+  ggplot(aes(
+    x = log10(otu_rel),
+    y = otu_occ,
+    fill = fill
+  )) +
+  scale_fill_npg(
+    name = "Core Membership",
+    labels = c("Core Taxa", "Non Core Taxa")
+  ) +
+  geom_point(pch = 21, alpha = 1, size = 2.5) +
+  labs(x = "log10(Mean Relative Abundance)", y = "Occupancy") +
+  theme_bw() +
+  theme(
+    axis.title.x = element_text(size = 14),
+    title = element_text(size = 14),
+    axis.title.y = element_text(size = 14),
+    strip.text.x = element_text(size = 10),
+    strip.text.y = element_text(size = 14),
+    axis.text.x = element_text(size = 12, color = "black"),
+    axis.text.y = element_text(size = 12, color = "black"),
+    legend.text = element_text(size = 12),
+    legend.title = element_text(size = 14),
+    plot.margin = unit(c(.5, 1, .5, .5), "cm")
+  )
 
 # Fit Abundance-Occupancy Distribution to a Neutral Model
 # Fit neutral model
@@ -115,117 +115,118 @@ sta.np <- sncm.fit(spp, taxon, stats = TRUE, pool = NULL)
 
 obs.np$fit_class <- "As predicted"
 obs.np[which(obs.np$freq < obs.np$pred.lwr), "fit_class"] <-
-    "Below prediction"
+  "Below prediction"
 obs.np[which(obs.np$freq > obs.np$pred.upr), "fit_class"] <-
-    "Above prediction"
+  "Above prediction"
 obs.np[which(is.na(obs.np$freq)), "fit_class"] <- "NA"
 
 obs.np <- tibble::rownames_to_column(obs.np, "OTU_ID")
 as.data.frame(left_join(occ_abun, obs.np, by = "OTU_ID")) -> fit_table
 #
 sta.np$above.pred <-
-    sum(obs.np$freq > (obs.np$pred.upr), na.rm = TRUE) / sta.np$Richness
+  sum(obs.np$freq > (obs.np$pred.upr), na.rm = TRUE) / sta.np$Richness
 sta.np$below.pred <-
-    sum(obs.np$freq < (obs.np$pred.lwr), na.rm = TRUE) / sta.np$Richness
+  sum(obs.np$freq < (obs.np$pred.lwr), na.rm = TRUE) / sta.np$Richness
 fit_res <- as.data.frame(sta.np)
 rownames(fit_res) <- "Value"
 fit_res
 obs2 <- as.data.frame(list_tab[[1]])
 
 obs1 <- obs1 %>%
-    mutate(fill_fit_class = paste0(fill, ":", fit_class))
+  mutate(fill_fit_class = paste0(fill, ":", fit_class))
 
 obs1 <- obs1 %>%
-    mutate(across(
-        "fill_fit_class",
-        str_replace,
-        "no:Below prediction",
-        "Non Core Taxa"
-    )) %>%
-    mutate(across(
-        "fill_fit_class",
-        str_replace,
-        "no:Above prediction",
-        "Non Core Taxa"
-    )) %>%
-    mutate(across(
-        "fill_fit_class",
-        str_replace,
-        "no:As predicted",
-        "Non Core Taxa"
-    ))
+  mutate(across(
+    "fill_fit_class",
+    str_replace,
+    "no:Below prediction",
+    "Non Core Taxa"
+  )) %>%
+  mutate(across(
+    "fill_fit_class",
+    str_replace,
+    "no:Above prediction",
+    "Non Core Taxa"
+  )) %>%
+  mutate(across(
+    "fill_fit_class",
+    str_replace,
+    "no:As predicted",
+    "Non Core Taxa"
+  ))
 
 
 obs1 %>%
-    ggplot(aes(x = log10(otu_rel), y = otu_occ)) +
-    scale_fill_npg(
-        name = "Core Membership: Model Predictions",
-        labels = c(
-            "Core: Above Prediction",
-            "Core: As Predicted",
-            "Core: Below Prediction",
-            "Non-Core Taxa" )
-    ) +
-    geom_point(
-        aes(fill = fill_fit_class),
-        pch = 21,
-        alpha = 0.75,
-        size = 2.2
-    ) +
-    geom_line(
-        color = "red",
-        data = obs1,
-        size = 0.8,
-        aes(y = obs1$freq.pred, x = log10(obs1$p)),
-        alpha = 0.55
-    ) +
-    geom_line(
-        color = "black",
-        lty = "twodash",
-        size = 0.9,
-        data = obs1,
-        aes(y = obs1$pred.upr, x = log10(obs1$p)),
-        alpha = 0.55
-    ) +
-    geom_line(
-        color = "black",
-        lty = "twodash",
-        size = 0.9,
-        data = obs1,
-        aes(y = obs1$pred.lwr, x = log10(obs1$p)),
-        alpha = 0.55
-    ) +
-    labs(x = "Log10(mean abundance)", y = "Occupancy") +
-    annotate(
-        "text",
-        -Inf,
-        Inf,
-        label = paste("italic(R)^2 ==", round(obs2$Rsqr, 3)),
-        parse = TRUE,
-        size = 4.8,
-        hjust = -0.2,
-        vjust = 1.2
-    ) +
-    annotate(
-        "text",
-        -Inf,
-        Inf,
-        label = paste("italic(m) ==", round(obs2$m, 3)),
-        parse = TRUE,
-        size = 4.8,
-        hjust = -0.2,
-        vjust = 3.2
-    ) +
-    theme_bw() +
-    theme(
-        axis.title.x = element_text(size = 14),
-        title = element_text(size = 14),
-        axis.title.y = element_text(size = 14),
-        strip.text.x = element_text(size = 10),
-        strip.text.y = element_text(size = 14),
-        axis.text.x = element_text(size = 12, color = "black"),
-        axis.text.y = element_text(size = 12, color = "black"),
-        legend.text = element_text(size = 12),
-        legend.title = element_text(size = 14),
-        plot.margin = unit(c(.5, 1, .5, .5), "cm")
+  ggplot(aes(x = log10(otu_rel), y = otu_occ)) +
+  scale_fill_npg(
+    name = "Core Membership: Model Predictions",
+    labels = c(
+      "Core: Above Prediction",
+      "Core: As Predicted",
+      "Core: Below Prediction",
+      "Non-Core Taxa"
     )
+  ) +
+  geom_point(
+    aes(fill = fill_fit_class),
+    pch = 21,
+    alpha = 0.75,
+    size = 2.2
+  ) +
+  geom_line(
+    color = "red",
+    data = obs1,
+    size = 0.8,
+    aes(y = obs1$freq.pred, x = log10(obs1$p)),
+    alpha = 0.55
+  ) +
+  geom_line(
+    color = "black",
+    lty = "twodash",
+    size = 0.9,
+    data = obs1,
+    aes(y = obs1$pred.upr, x = log10(obs1$p)),
+    alpha = 0.55
+  ) +
+  geom_line(
+    color = "black",
+    lty = "twodash",
+    size = 0.9,
+    data = obs1,
+    aes(y = obs1$pred.lwr, x = log10(obs1$p)),
+    alpha = 0.55
+  ) +
+  labs(x = "Log10(mean abundance)", y = "Occupancy") +
+  annotate(
+    "text",
+    -Inf,
+    Inf,
+    label = paste("italic(R)^2 ==", round(obs2$Rsqr, 3)),
+    parse = TRUE,
+    size = 4.8,
+    hjust = -0.2,
+    vjust = 1.2
+  ) +
+  annotate(
+    "text",
+    -Inf,
+    Inf,
+    label = paste("italic(m) ==", round(obs2$m, 3)),
+    parse = TRUE,
+    size = 4.8,
+    hjust = -0.2,
+    vjust = 3.2
+  ) +
+  theme_bw() +
+  theme(
+    axis.title.x = element_text(size = 14),
+    title = element_text(size = 14),
+    axis.title.y = element_text(size = 14),
+    strip.text.x = element_text(size = 10),
+    strip.text.y = element_text(size = 14),
+    axis.text.x = element_text(size = 12, color = "black"),
+    axis.text.y = element_text(size = 12, color = "black"),
+    legend.text = element_text(size = 12),
+    legend.title = element_text(size = 14),
+    plot.margin = unit(c(.5, 1, .5, .5), "cm")
+  )
