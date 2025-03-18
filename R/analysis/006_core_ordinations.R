eea56c07a1b09a0f372b72a8275cc658427192de
Bolívar Aponte Rolón <64099468+jibarozzo@users.noreply.github.com>
2025-03-18 15:01:25 -0500

    Revert "Gains changes from develop by #31 (#34)"
    This reverts commit b1bdca2f74ec7e73e376e1cea433808ac54443e7.

--- R/analysis/006_core_ordinations.R
+++ R/analysis/006_core_ordinations.R
@@ -4,16 +4,21 @@
 
 # Setup
 source("R/utils/000_setup.R")
+load("data/output/core_summary_lists.rda")
+load("data/output/phyloseq_objects/filtered_phyloseq.rda")
+load("data/output/phyloseq_objects/core_brc_phyloseq.rda")
+load("data/output/phyloseq_objects/non_core_brc_phyloseq.rda")
+load("data/output/core_asv_matrix.rda")
+load("data/output/non_core_asv_matrix.rda")
+
 
 #################################################
 ###### Ordinations and Community Analysis #######
 #################################################
 
-##############################
-### Core by ExctractCore() ###
-##############################
-# Choosing the number of dimensions for NMDS
-
+############
+### Core ###
+############
 # Hellinger transformation of matrices
 # To be used for NMDS, dbRDA and adonis2
 core_hell_matrix <- decostand(t(core_asv_matrix),
@@ -21,6 +26,7 @@ core_hell_matrix <- decostand(t(core_asv_matrix),
   method = "hellinger"
 ) # Now we need samples as columns and ASV are rows
 
+###########
 
 # Ordinations
 core_asv_dist <- vegdist(t(core_hell_matrix),
@@ -30,48 +36,151 @@ core_asv_dist <- vegdist(t(core_hell_matrix),
   na.rm = TRUE
 )
 
-# set.seed(484035)
-# nmds_screen_parallel(core_asv_dist, ncores = 2) # Results: Two dimensions keeps stress below 0.20
-#################
 
-core_ext_core <- calculate_nmds(
-  asv_matrix = core_asv_matrix,
-  physeq = core_brc_phyloseq,
-  ncores = parallel::detectCores(),
-  k = 3,
-  trymax = 9999
-) # Best solution repeated 1 times (k = 3)
+## Choosing the number of dimensions
+# set.seed(484035)
+# nmds_screen_parallel(core_asv_dist, ncores = 32) # Results: Two dimensions keeps stress below 0.20
+
+
+core_ordi <- metaMDS(as.matrix(core_asv_dist),
+  distance = "bray",
+  display = c("sites"),
+  noshare = TRUE,
+  autotransform = FALSE,
+  wascores = TRUE,
+  tidy = TRUE,
+  k = 2,
+  trymax = 100,
+  parallel = parallel::detectCores()
+)
+
+stressplot(core_ordi)
+
+## Scores and sample data for NMDS
+vegan::sppscores(core_ordi) <- t(core_hell_matrix)
+
+core_nmds_scores <- as.data.frame(vegan::scores(core_ordi)$sites)
+
+rownames(core_nmds_scores) <- rownames(vegan::scores(core_ordi)$sites)
+
+core_nmds_scores <- core_nmds_scores %>%
+  rownames_to_column(., var = "unique_id")
+
+core_brc_sample_df <- core_brc_phyloseq@sam_data %>%
+  data.frame() %>%
+  rownames_to_column(., var = "unique_id")
+
+core_nmds_df <- right_join(core_brc_sample_df, core_nmds_scores, by = "unique_id")
 
 
 # Core NMDS Aesthetics ####
-core_nmds_crops <- gg_nmds(
-  .data = core_ext_core$nmds_df,
+core_nmds_crops <- core_nmds(
+  .data = core_nmds_df,
   .color = crop,
   .drop_na = brc
-) + ggtitle("50 core ASVs in BRC crops",
-  subtitle = "Core that contributes 2% to Bray-Curtis"
-)
+) + ggtitle("50 core ASVs in BRC crops (100% samples)")
 
-core_nmds_brc <- gg_nmds(
-  .data = core_ext_core$nmds_df,
+core_nmds_brc <- core_nmds(
+  .data = core_nmds_df,
   .color = brc,
   .drop_na = brc
-) + ggtitle("50 core ASVs in BRC crops",
-  subtitle = "Core that contributes 2% to Bray-Curtis"
+) + ggtitle("50 core ASVs in BRCs (100% samples)")
+
+
+ggsave(
+  filename = "core_nmds_crops.png",
+  plot = core_nmds_crops,
+  path = "data/output/plots/",
+  dpi = 300,
+  width = 200,
+  height = 200,
+  units = "mm"
+)
+
+ggsave(
+  filename = "core_nmds_brc.png",
+  plot = core_nmds_brc,
+  path = "data/output/plots/",
+  dpi = 300,
+  width = 200,
+  height = 200,
+  units = "mm"
+)
+
+# Threshold analysis
+
+# Calculate occurrence of each ASV across samples
+asv_sample_counts <- colSums(core_asv_matrix > 0)
+
+# Get total number of samples
+total_samples <- nrow(core_asv_matrix)
+
+# Define occurrence threshold (60% of samples)
+threshold_60 <- total_samples * 0.6
+
+# Select ASVs based on occurrence criteria
+high_occurrence_asvs <- names(asv_sample_counts[asv_sample_counts >= threshold_60])
+low_occurrence_asvs <- names(asv_sample_counts[asv_sample_counts < threshold_60])
+
+# Print ASV counts
+cat("ASVs found in ≥60% samples:", length(high_occurrence_asvs), "\n")
+cat("ASVs found in <60% samples:", length(low_occurrence_asvs), "\n")
+
+
+
+
+
+################
+### Non-Core ###
+################
+
+# Hellinger transformation of matrices
+non_core_hell_matrix <- decostand(t(non_core_asv_matrix),
+  MARGIN = 1,
+  method = "hellinger"
+) # Now we need samples as columns and ASV are rows
+
+###########
+
+# Ordinations
+non_core_asv_dist <- vegdist(t(non_core_hell_matrix),
+  method = "bray",
+  upper = FALSE,
+  binary = FALSE,
+  na.rm = TRUE
 )
 
 
-##################################
-### Non-Core by ExctractCore() ###
-##################################
-
-non_core_ext_core <- calculate_nmds(
-  asv_matrix = non_core_asv_matrix,
-  physeq = non_core_brc_phyloseq,
-  ncores = 1,
-  k = 3,
-  trymax = 9999
+non_core_ordi <- metaMDS(as.matrix(non_core_asv_dist),
+  distance = "bray",
+  display = c("sites"),
+  noshare = TRUE,
+  autotransform = FALSE,
+  wascores = TRUE,
+  zerodist = "ignore",
+  tidy = TRUE,
+  k = 2,
+  trymax = 100,
+  parallel = parallel::detectCores()
 )
+
+stressplot(non_core_ordi)
+
+## Scores and sample data for NMDS
+vegan::sppscores(non_core_ordi) <- t(non_core_hell_matrix)
+
+non_nmds_scores <- as.data.frame(vegan::scores(non_core_ordi)$sites)
+
+rownames(non_nmds_scores) <- rownames(vegan::scores(non_core_ordi)$sites)
+
+non_nmds_scores <- non_nmds_scores %>%
+  rownames_to_column(., var = "unique_id")
+
+non_core_brc_sample_df <- non_core_brc_phyloseq@sam_data %>%
+  data.frame() %>%
+  rownames_to_column(., var = "unique_id")
+
+non_core_nmds_df <- right_join(non_core_brc_sample_df, non_nmds_scores, by = "unique_id")
 
 # Non-Core NMDS Aesthetics ####
 non_core_nmds_crops <- core_nmds(
@@ -89,52 +198,22 @@ non_core_nmds_brc <- core_nmds(
 
 
 
-######################################
-### Core and Non-Core by Threshold ###
-######################################
-
-high_occ_core <- calculate_nmds(
-    asv_matrix = physeq_high_occ_matrix,
-    physeq = physeq_high_occ,
-    ncores = parallel::detectCores(),
-    k = 3,
-    trymax = 9999
+ggsave(
+  filename = "non_core_nmds_crops.png",
+  plot = non_core_nmds_crops,
+  path = "data/output/plots/",
+  dpi = 300,
+  width = 200,
+  height = 200,
+  units = "mm"
 )
 
-
-# Core NMDS Aesthetics ####
-high_occ_nmds_crops <- gg_nmds(
-  .data = high_occ_nmds_df,
-  .color = crop,
-  .drop_na = brc
-) + ggtitle("12 core ASVs in BRC crops (60% samples)")
-
-high_occ_nmds_brc <- core_nmds(
-  .data = core_nmds_df,
-  .color = brc,
-  .drop_na = brc
-) + ggtitle("50 core ASVs in BRCs (100% samples)")
-
-
-
-## Saving plots
-core_plots <- list(core_nmds_brc, core_nmds_crops)
-plot_names <- c(
-  "core_nmds_brc",
-  "core_nmds_crops"
-)
-
-
-plot_paths <- str_glue("data/output/plots/{tolower(plot_names)}.png")
-
-purrr::walk2(
-  plot_paths, core_plots,
-  \(path, plot) ggsave(
-    filename = path,
-    plot = plot,
-    dpi = 300,
-    width = 200,
-    height = 200,
-    units = "mm"
-  )
+ggsave(
+  filename = "non_core_nmds_brc.png",
+  plot = non_core_nmds_brc,
+  path = "data/output/plots/",
+  dpi = 300,
+  width = 200,
+  height = 200,
+  units = "mm"
 )
--- R/utils/000_setup.R
+++ R/utils/000_setup.R
@@ -1,42 +1,21 @@
 # Package and Environment setup
-if (!require("pacman"))
-    install.packages("pacman")
+# Bolívar Aponte Rolón
 
-pacman::p_load(
-    conflicted,
-    styler,
-    phyloseq,
-    vegan,
-    tidyverse,
-    ggsci,
-    ggpubr,
-    hillR,
-    microViz,
-    magrittr,
-    lazyeval,
-    minpack.lm,
-    Hmisc,
-    stats4,
-    metagMisc
-)
+library(phyloseq) # Bioconductor v1.50.0
+library(vegan) # CRAN v2.6-10
+library(tidyverse) # Posit RPSM v2.0.0
+library(ggsci) # [standard::NA/NA] v3.2.0
+library(ggpubr) # [standard::NA/NA] v0.6.0
+library(hillR) # [standard::NA/NA] v0.5.2
+library(microViz) # [::NA/NA] v0.12.6
+library(magrittr) # Posit RPSM v2.0.3
+library(lazyeval) # Posit RPSM v0.2.2 # Deprecated. Change to rlang
+library(minpack.lm) # [standard::NA/NA] v1.2-4
+library(Hmisc) # [standard::NA/NA] v5.2-2
+library(stats4) # local install v4.4.1
+library(metagMisc) # [github::vmikk/metagMisc] v0.5.0
 
-# Scripts
-## List files and source each
-list.files("R/functions/", full.names = TRUE) %>%
-  purrr::map(source)
 
-# Objects
-list.files("data/output",
-  full.names = TRUE,
-  recursive = TRUE,
-  pattern = "\\.rda$"
-) %>%
-  purrr::walk(., load, envir = .GlobalEnv)
-
-# Solve known conflicts
-conflict_prefer("select", "dplyr")
-conflict_prefer("filter", "dplyr")
-conflict_prefer("rename", "dplyr")
-conflict_prefer("mutate", "dplyr")
-conflict_prefer("intersect", "base")
-conflict_prefer("survival", "cluster")
+source("R/functions/extract_core.R")
+source("R/functions/model_fit.R")
+source("R/functions/misc_functions.R")
