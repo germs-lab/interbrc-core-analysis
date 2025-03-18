b1bdca2f74ec7e73e376e1cea433808ac54443e7
Bolívar Aponte Rolón <64099468+jibarozzo@users.noreply.github.com>
2025-03-18 14:11:43 -0500

    Gains changes from develop by #31 (#34)
    * Refactored  BC_ranked object generation. Updated `core_summary_lists.rda` calls throughout 004_core_analysis.R
    
    * ExtractCore() gains named element list at `return_list`.
    - Tested with 100 samples
    - Results: `spatial_core$core_otus == spatial_core[[1]`
    - Updated path for `utils/000_setup.R` in `test_analysis.R`
    
    * Improvements on abundance threshold analysis.
    
    * Attempt at functionallizing selection of ASVs
    
    * New function to select ASVs by threshold
    
    * Core community selected by 60% threshold. Gain `select_asv()`
    
    * NMDS for core ASVs obtained at 60% threshold.
    
    * Changed loading of .rda files to 000_setup.R. THe rest of the fils just source the setup file.
    
    * Reverted the ranked_bc() calculation back to its original format
    
    Both 'x_name's and 'z' need to be called first before the for loop to ensure it runs properly. The for loop has been changed back to its original format due to issues with larger datasets -- with the goal of refactoring/parallelizing this loop next.
    
    * Gains `calculate_nmds()` to obtain NMDS values. Name change for `core_nmds()` >> `gg_nmds()`
    
    * Changes to names of miscellaneous functions to snake_case.
    
    * Using calculate_nmds() to refactor analysis in 006_core_ordinations
    
    * Draft of test for `calculate_nmds.R`
    
    * Test successful. Needs new test for tests 5 and 6.
    
    * Test 5, 6 and 8 for `calculate_nmds()` complete and successful. New `expected_nmds.rda `for comparing results
    
    * New tests for `extract_core` and `filter_core`. Updated `renv.lock`
    
    * Gains `expected_streact_core.rda`. Gains passing tests for `extract_core.R`
    
    * Gains `test.yml` for GH actions.
    
    * Update .Rbuildignore
    
    * Update test.yml
    
    * Update renv.lock
    
    * Update renv.lock
    
    * GH actions tests using DESCRIPTION
    
    * Restoring R dependencies via renv:restore()
    
    * Removed `pak` from tests.yml
    
    * Removed DESCRIPTION
    
    * Test directory updated to `tests/testthat/`
    
    * New file paths with `here::here()`
    
    * Update renv.lock withe `here`
    
    * Linux only dependencies
    
    * Update for system level dependencies for Linux runners.
    
    * Development versions of dependencies
    
    * Typo fix
    
    * Removed older R version tests from tests.yml
    
    * Gains `test-filter_core.R`. Improved error messaged in `filter_core` when handling orientation of taxa. Calculates occurrence of ASVs across samples and total number of samples correctly according to the orientation of the taxa. (#32)
    
    * Improvements to NMDS from threshold analysis and `multi_rarefy` (#33)
    
    * Threshdold analysis improvements. NMDS analyses do not converge.
    
    * Updated NMDS plost for extract_core Core.
    
    * Resolved conflicts and applied stashed changes
    
    * multi rarefaction function
    
    * New patch with R/utils/000_setup.R (#35)
    
    Co-authored-by: Gian77
    
    * Update 000_setup.R. Re-gains R object loading.
    
    ---------
    
    Co-authored-by: Gian77
    
    ---------
    
    Co-authored-by: B-Kristy

--- R/functions/misc_functions.R
+++ R/functions/misc_functions.R
@@ -1,9 +1,9 @@
 # Miscellaneous functions for Inter-BRC Core Microbiome Analysis
 # By Bolívar Aponte Rolón
 
-# ExtracMatrix(): extract/subset a ASV/OTU matrix based on a vector of strings and a rowSums() condition.
+# extract_matrix(): extract/subset a ASV/OTU matrix based on a vector of strings and a rowSums() condition.
 
-ExtractMatrix <- function(physeq, .vec, keep_rows_sums = 0) {
+extract_matrix <- function(physeq, .vec, keep_rows_sums = 0) {
   # Error handling
   if (!inherits(physeq, c("phyloseq", "matrix"))) {
     cli::cli_abort(
@@ -53,7 +53,7 @@ ExtractMatrix <- function(physeq, .vec, keep_rows_sums = 0) {
 #' @author Guillem Salazar <salazar@@icm.csic.es>
 #' @examples
 #' subset.fasta(file = "http://greengenes.lbl.gov/Data/JD_Tutorial/UnAligSeq24606.txt", subset = c("24.6jsd1.Tut", "24.6jsd2.Tut ", "24.6jsd3.Tut "), out = "out.fasta")
-subset.fasta <- function(file = NULL, subset = NULL, out = paste(file, ".subset", sep = "")) {
+subset_fasta <- function(file = NULL, subset = NULL, out = paste(file, ".subset", sep = "")) {
   library(Biostrings)
   sequences <- readDNAStringSet(file)
   if (all(as.character(subset) %in% names(sequences)) == FALSE) stop("There are names in 'subset' not present in the fasta file")
@@ -66,7 +66,7 @@ subset.fasta <- function(file = NULL, subset = NULL, out = paste(file, ".subset"
 nmds_screen_parallel <- function(x, ncores = parallel::detectCores() - 1) {
   # Function to calculate stress for a given number of dimensions
   calculate_stress <- function(k) {
-    replicate(10, metaMDS(x, autotransform = FALSE, k = k)$stress)
+    replicate(10, metaMDS(x, autotransform = FALSE, k = k, maxit = 100, trymax = 10)$stress)
   }
 
   # Use mclapply to parallelize the stress calculation
@@ -87,7 +87,7 @@ nmds_screen_parallel <- function(x, ncores = parallel::detectCores() - 1) {
 }
 
 # Standard NMDS plots
-core_nmds <- function(.data, .color, .shape = NULL, .drop_na) {
+gg_nmds <- function(.data, .color, .shape = NULL, .drop_na) {
   .color <- enquo(.color)
   .drop_na <- enquo(.drop_na)
 
