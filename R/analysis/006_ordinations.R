97b5db59ab591485ee5424cedaa1822b846da332
jibarozzo <bolaponte@pm.me>
2025-03-24 15:50:53 -0500

    Gains new `calculate_pcoa()`. Plots for PCoA completed.

--- R/analysis/006_ordinations.R
+++ R/analysis/006_ordinations.R
@@ -9,33 +9,6 @@ remove(phyloseq)
 #################################################
 ###### Ordinations and Community Analysis #######
 #################################################
-# List of transformed matrices
-# # Transformed matrices
-# hell_matrices <- purrr::map(asv_matrices, ~ {
-#   decostand(t(.x), method = "hellinger", MARGIN = 1)
-# })
-
-# # Distance matrices
-# plan(multisession) # Set up parallel backend
-
-# distance_matrices <- hell_matrices %>%
-#   future_map(~ vegdist(t(.x), method = "bray", na.rm = TRUE),
-#     .options = furrr_options(seed = TRUE)
-#   ) %>%
-#   set_names(names(hell_matrices))
-
-
-# distance_matrices <- purrr::map(
-#   .x = hell_matrices, ~
-#     vegdist(t(.x),
-#       method = "bray",
-#       upper = FALSE,
-#       binary = FALSE,
-#       na.rm = TRUE
-#     )
-# )
-
-
 ##############################
 ### Core by ExctractCore() ###
 ##############################
@@ -118,17 +91,17 @@ high_occ_nmds <- calculate_nmds(
 # save(high_occ_nmds, file = "data/output/high_occ_nmds.rda")
 
 
-high_occ_nmds_crops <- gg_nmds(
+high_nmds_crops <- gg_nmds(
   .data = high_occ_nmds$nmds_df,
   .color = crop,
   .drop_na = brc
-) + ggtitle("12 core ASVs in BRC crops (60% samples)")
+) + ggtitle("12 high prevanlence ASVs in BRC crops (60% samples)")
 
-high_occ_nmds_brc <- gg_nmds(
+high_nmds_brc <- gg_nmds(
   .data = high_occ_nmds$nmds_df,
   .color = brc,
   .drop_na = brc
-) + ggtitle("Non-Core ASVs in BRCs (60% samples)")
+) + ggtitle("12 high prevalence ASVs in BRCs (60% samples)")
 
 
 # Low occupancy
@@ -143,39 +116,39 @@ low_occ_nmds <- calculate_nmds(
 save(low_occ_nmds, file = "data/output/low_occ_nmds.rda")
 
 
-low_occ_nmds_crops <- gg_nmds(
+low_nmds_crops <- gg_nmds(
   .data = low_occ_nmds$nmds_df,
   .color = crop,
   .drop_na = brc
-) + ggtitle("12 core ASVs in BRC crops (60% samples)")
+) + ggtitle("Low prevalence ASVs in BRC crops (<60% samples)")
 
-low_occ_nmds_brc <- gg_nmds(
+low_nmds_brc <- gg_nmds(
   .data = low_occ_nmds$nmds_df,
   .color = brc,
   .drop_na = brc
-) + ggtitle("Non-Core ASVs in BRCs (<60% samples)")
+) + ggtitle("Low prevalence ASVs in BRCs (<60% samples)")
 
 
 ## Saving plots
 
-core_plots <- list(
+nmds_plots <- list(
   core_nmds_brc, core_nmds_crops,
   non_core_nmds_brc, non_core_nmds_crops,
-  high_occ_nmds_crops, high_occ_nmds_brc,
-  low_occ_nmds_crops, low_occ_nmds_brc
+  high_nmds_crops, high_nmds_brc,
+  low_nmds_crops, low_nmds_brc
 )
 plot_names <- c(
   "core_nmds_brc", "core_nmds_crops",
   "non_core_nmds_brc", "non_core_nmds_crops",
   "high_occ_nmds_crops", "high_occ_nmds_brc",
-  "low_occ_nmds_crops", "low_occ_nmds_brc"
+  "low_nmds_crops", "low_nmds_brc"
 )
 
 
 plot_paths <- str_glue("data/output/plots/{tolower(plot_names)}.png")
 
 purrr::walk2(
-  plot_paths, core_plots,
+  plot_paths, nmds_plots,
   \(path, plot) ggsave(
     filename = path,
     plot = plot,
@@ -191,49 +164,246 @@ purrr::walk2(
 ############
 ### PCoA ###
 ############
-# List of transformed matrices
-
+# Transformed matrices
 hell_matrices <- purrr::map(asv_matrices, ~ {
   decostand(t(.x), method = "hellinger", MARGIN = 1)
 })
 
-# Distances
-distance_matrices <- purrr::map(hell_matrices ~
-  vegdist(t(.x),
-    method = "bray",
-    upper = FALSE,
-    binary = FALSE,
-    na.rm = TRUE
-  ))
+# Distance matrices
+plan(multicore, workers = parallel::detectCores() - 1) # Set up parallel backend
 
 distance_matrices <- hell_matrices %>%
-  purrr::map(~ vegdist(t(.x), method = "bray", na.rm = TRUE),
-    .progress = TRUE
+  future_map(
+    ~ vegdist(t(.x),
+      method = "bray",
+      upper = FALSE,
+      binary = FALSE,
+      na.rm = TRUE
+    ),
+    .progress = TRUE,
+    .options = furrr_options(seed = TRUE)
   ) %>%
-  set_names(names(hell_matrices))
+  purrr::set_names(names(hell_matrices))
 
+plan(sequential) # Close background work by changing plans
 
+##############################
+### Core by extract_core() ###
+##############################
+core_asv_pcoa <- calculate_pcoa(
+  distance_matrices$core,
+  core_brc_phyloseq
+) # Need a distance matric from vegdist()
 
-core_asv_pcoa <- wcmdscale(core_asv_dist,
-  k = 2,
-  eig = TRUE,
-  add = FALSE,
-  x.ret = FALSE
-)
-
-# Extract PCoA scores and add metadata
-core_pcoa_df <- data.frame(
-  Dim1 = core_asv_pcoa$points[, 1], # First PCoA axis
-  Dim2 = core_asv_pcoa$points[, 2], # Second PCoA axis
-  brc = factor(core_brc_phyloseq@sam_data$brc), # Convert 'brc' to a factor
-  crop = factor(core_brc_phyloseq@sam_data$crop)
-)
-
-
-core_pcoa_gg <- ggplot(data = core_pcoa_df, aes(x = Dim1, y = Dim2, color = brc)) +
+core_pcoa_brc <- ggplot(data = core_asv_pcoa$pcoa_df, aes(x = Dim1, y = Dim2, color = brc)) +
   geom_point() +
   theme_bw() +
   labs(color = "BRC") +
+  stat_ellipse(
+    aes(color = brc),
+    geom = "path",
+    linewidth = 1.3,
+    position = "identity",
+    type = "t",
+    linetype = 1,
+    level = 0.95,
+    segments = 51,
+    na.rm = FALSE,
+    show.legend = NA,
+    inherit.aes = TRUE
+  ) +
+  ggtitle("PCoA: 50 core ASVs in BRC",
+    subtitle = "Core that contributes 2% to Bray-Curtis"
+  )
+
+core_pcoa_crops <- ggplot(data = core_pcoa_df, aes(x = Dim1, y = Dim2, color = crop)) +
+  geom_point() +
+  theme_bw() +
+  labs(color = "Crop") +
+  stat_ellipse(
+    aes(color = crop),
+    geom = "path",
+    linewidth = 1.3,
+    position = "identity",
+    type = "t",
+    linetype = 1,
+    level = 0.95,
+    segments = 51,
+    na.rm = FALSE,
+    show.legend = NA,
+    inherit.aes = TRUE
+  ) +
   ggtitle("PCoA: 50 core ASVs in BRC crops",
     subtitle = "Core that contributes 2% to Bray-Curtis"
   )
+
+
+##################################
+### Non-Core by extract_core() ###
+##################################
+non_core_asv_pcoa <- calculate_pcoa(
+  distance_matrices$non_core,
+  non_core_brc_phyloseq
+)
+
+non_core_pcoa_brc <- ggplot(data = non_core_asv_pcoa$pcoa_df, aes(x = Dim1, y = Dim2, color = brc)) +
+  geom_point() +
+  theme_bw() +
+  labs(color = "BRC") +
+  stat_ellipse(
+    aes(color = brc),
+    geom = "path",
+    linewidth = 1.3,
+    position = "identity",
+    type = "t",
+    linetype = 1,
+    level = 0.95,
+    segments = 51,
+    na.rm = FALSE,
+    show.legend = NA,
+    inherit.aes = TRUE
+  ) +
+  ggtitle("PCoA: Non-Core ASVs in BRCs")
+
+non_core_pcoa_crops <- ggplot(data = non_core_asv_pcoa$pcoa_df, aes(x = Dim1, y = Dim2, color = crop)) +
+  geom_point() +
+  theme_bw() +
+  labs(color = "Crop") +
+  stat_ellipse(
+    aes(color = crop),
+    geom = "path",
+    linewidth = 1.3,
+    position = "identity",
+    type = "t",
+    linetype = 1,
+    level = 0.95,
+    segments = 51,
+    na.rm = FALSE,
+    show.legend = NA,
+    inherit.aes = TRUE
+  ) +
+  ggtitle("PCoA: Non-Core ASVs in BRC crops")
+
+
+## High occupancy
+
+high_asv_pcoa <- calculate_pcoa(
+  distance_matrices$high_occ,
+  core_asvs_threshold$physeq_high_occ %>%
+    prune_samples(sample_sums(.) > 0, .) # Making the matrix and phyloseq the same: 1732 samples
+)
+
+high_pcoa_brc <- ggplot(data = high_asv_pcoa$pcoa_df, aes(x = Dim1, y = Dim2, color = brc)) +
+  geom_point() +
+  theme_bw() +
+  labs(color = "BRC") +
+  stat_ellipse(
+    aes(color = brc),
+    geom = "path",
+    linewidth = 1.3,
+    position = "identity",
+    type = "t",
+    linetype = 1,
+    level = 0.95,
+    segments = 51,
+    na.rm = FALSE,
+    show.legend = NA,
+    inherit.aes = TRUE
+  ) +
+  ggtitle("PCoA: High prevalence ASVs in BRCs")
+
+high_pcoa_crops <- ggplot(data = high_asv_pcoa$pcoa_df, aes(x = Dim1, y = Dim2, color = crop)) +
+  geom_point() +
+  theme_bw() +
+  labs(color = "Crop") +
+  stat_ellipse(
+    aes(color = crop),
+    geom = "path",
+    linewidth = 1.3,
+    position = "identity",
+    type = "t",
+    linetype = 1,
+    level = 0.95,
+    segments = 51,
+    na.rm = FALSE,
+    show.legend = NA,
+    inherit.aes = TRUE
+  ) +
+  ggtitle("PCoA: High prevalence ASVs in BRC crops")
+
+## Low prevalence
+
+
+low_asv_pcoa <- calculate_pcoa(
+  distance_matrices$low_occ,
+  core_asvs_threshold$physeq_low_occ %>%
+    prune_samples(sample_sums(.) > 0, .) # Making the matrix and phyloseq the same: 1732 samples
+)
+
+low_pcoa_brc <- ggplot(data = low_asv_pcoa$pcoa_df, aes(x = Dim1, y = Dim2, color = brc)) +
+  geom_point() +
+  theme_bw() +
+  labs(color = "BRC") +
+  stat_ellipse(
+    aes(color = brc),
+    geom = "path",
+    linewidth = 1.3,
+    position = "identity",
+    type = "t",
+    linetype = 1,
+    level = 0.95,
+    segments = 51,
+    na.rm = FALSE,
+    show.legend = NA,
+    inherit.aes = TRUE
+  ) +
+  ggtitle("PCoA: Low prevalence ASVs in BRCs")
+
+low_pcoa_crops <- ggplot(data = low_asv_pcoa$pcoa_df, aes(x = Dim1, y = Dim2, color = crop)) +
+  geom_point() +
+  theme_bw() +
+  labs(color = "Crop") +
+  stat_ellipse(
+    aes(color = crop),
+    geom = "path",
+    linewidth = 1.3,
+    position = "identity",
+    type = "t",
+    linetype = 1,
+    level = 0.95,
+    segments = 51,
+    na.rm = FALSE,
+    show.legend = NA,
+    inherit.aes = TRUE
+  ) +
+  ggtitle("PCoA: Low prevalence ASVs in BRC crops")
+
+
+## Saving PCoAs
+pcoa_plots <- list(
+  core_pcoa_brc, core_pcoa_crops,
+  non_core_pcoa_brc, non_core_pcoa_crops,
+  high_pcoa_crops, high_pcoa_brc,
+  low_pcoa_crops, low_pcoa_brc
+)
+plot_names <- c(
+  "core_pcoa_brc", "core_pcoa_crops",
+  "non_core_pcoa_brc", "non_core_pcoa_crops",
+  "high_pcoa_crops", "high_pcoa_brc",
+  "low_pcoa_crops", "low_pcoa_brc"
+)
+
+
+plot_paths <- str_glue("data/output/plots/{tolower(plot_names)}.png")
+
+purrr::walk2(
+  plot_paths, pcoa_plots,
+  \(path, plot) ggsave(
+    filename = path,
+    plot = plot,
+    dpi = 300,
+    width = 200,
+    height = 200,
+    units = "mm"
+  )
+)
